fb5bb782c26e839de435ef1830741df4c560d533
Gil Benezer <gil.benezer@gmail.com>
2026-08-11 14:35:56 -0400

    implemented the forward and update portions of the critic network

--- src/networks/critics.py
+++ src/networks/critics.py
@@ -35,18 +35,19 @@ class ValueCritic(nn.Module):
         )
 
     def forward(self, obs: torch.Tensor) -> torch.Tensor:
-        # TODO: implement the forward pass of the critic network
-        pass
+        return self.network(obs)
 
     def update(self, obs: np.ndarray, q_values: np.ndarray) -> dict:
-        obs = ptu.from_numpy(obs)
-        q_values = ptu.from_numpy(q_values)
-
-        # TODO: compute the loss using the observations and q_values
-        loss = None
+        
+        obs_tensor = torch.from_numpy(obs).float().to(ptu.device)
+        q_values_tensor = torch.from_numpy(q_values).float().to(ptu.device)
+        
+        critic_value_tensor = self.network.forward(input=obs_tensor)
+        
+        loss = F.smooth_l1_loss(input=critic_value_tensor, target=q_values_tensor)
 
         # TODO: perform an optimizer step
-        pass
+        loss.backward()
 
         return {
             "Baseline Loss": loss.item(),
